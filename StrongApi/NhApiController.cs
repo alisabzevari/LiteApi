615cc47d28b02c368c1e8d034a5ebc5c8bf93fa4
Ali Sabzevari <ali@tidm.ir>
2015-05-30 15:46:17 +0430

    Write initial code for NhStrongApiController

---
+++ StrongApi/NhApiController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NHibernate;
+
+namespace StrongApi
+{
+    public class NhApiController : ApiController
+    {
+        protected readonly ISession Session;
+        protected readonly ITransaction Transaction;
+
+        public NhApiController(ISession session)
+        {
+            Session = session;
+            Transaction = Session.BeginTransaction();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Transaction.Dispose();
+                Session.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        protected virtual void TryCommit()
+        {
+            try
+            {
+                Transaction.Commit();
+            }
+            catch (Exception)
+            {
+                Transaction.Rollback();
+                throw;
+            }
+        }
+    }
+}
---
+++ StrongApi/NhStrongApiController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NHibernate;
+
+namespace StrongApi
+{
+    public class NhStrongApiController<TDto, TEntity> : NhApiController
+    {
+        public NhStrongApiController(ISession session)
+            : base(session)
+        {
+        }
+
+        public virtual IHttpActionResult Get(object id)
+        {
+            var entity = Session.Get<TEntity>(id);
+            if (entity == null)
+                return NotFound();
+            return Ok(entity);
+        }
+
+        public virtual IHttpActionResult Post(TDto dto)
+        {
+            var entity = MapToEntity(dto);
+            Session.Save(entity);
+            TryCommit();
+            return Created(GetLocation(entity), entity);
+        }
+        public IHttpActionResult Put(object id, [FromBody] TDto dto)
+        {
+            var oldEntity = Session.Get<TEntity>(id);
+            if (oldEntity == null)
+                return NotFound();
+            ReplaceEntityData(ref oldEntity, dto);
+            Session.Update(oldEntity);
+            TryCommit();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(object id)
+        {
+            var entity = Session.Get<TEntity>(id);
+            if (entity == null)
+                return NotFound();
+            Session.Delete(entity);
+            TryCommit();
+            return Ok();
+        }
+
+        protected virtual void ReplaceEntityData(ref TEntity oldEntity, TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual Uri GetLocation(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual TEntity MapToEntity(TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
