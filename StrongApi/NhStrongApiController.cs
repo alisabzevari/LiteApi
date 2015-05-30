9d68fa72dd158e499bd2438c667fe5dedce1aa74
Ali Sabzevari <ali@tidm.ir>
2015-05-30 17:14:57 +0430

    Added methods for query

--- StrongApi/NhStrongApiController.cs
+++ StrongApi/NhStrongApiController.cs
@@ -5,24 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using NHibernate;
+using NHibernate.Linq;
+
+//TODO: route actions
+//TODO: implement ReplaceEntityData
+//TODO: implement GetLocation
+//TODO: implement MapToEntity
 
 namespace StrongApi
 {
-    public class NhStrongApiController<TDto, TEntity> : NhApiController
+    public class NhStrongApiController<TDto, TEntity, TQueryDescriptor> : NhApiController
     {
         public NhStrongApiController(ISession session)
             : base(session)
-        {
-        }
+        { }
 
+        public virtual IHttpActionResult Get(TQueryDescriptor query)
+        {
+            var q = Session.Query<TEntity>();
+            return Ok(MapToDtoCollection(ExecuteQuery(q, query)));
+        }
         public virtual IHttpActionResult Get(object id)
         {
             var entity = Session.Get<TEntity>(id);
             if (entity == null)
                 return NotFound();
-            return Ok(entity);
+            return Ok(MapToDto(entity));
         }
-
         public virtual IHttpActionResult Post(TDto dto)
         {
             var entity = MapToEntity(dto);
@@ -40,7 +49,6 @@ namespace StrongApi
             TryCommit();
             return Ok();
         }
-
         public IHttpActionResult Delete(object id)
         {
             var entity = Session.Get<TEntity>(id);
@@ -50,21 +58,29 @@ namespace StrongApi
             TryCommit();
             return Ok();
         }
-
         protected virtual void ReplaceEntityData(ref TEntity oldEntity, TDto dto)
         {
             throw new NotImplementedException();
         }
-
         protected virtual Uri GetLocation(TEntity entity)
         {
             throw new NotImplementedException();
         }
-
         protected virtual TEntity MapToEntity(TDto dto)
         {
             throw new NotImplementedException();
         }
-
+        protected virtual ICollection<TEntity> ExecuteQuery(IQueryable<TEntity> q, TQueryDescriptor query)
+        {
+            throw new NotImplementedException();
+        }
+        protected virtual ICollection<TDto> MapToDtoCollection(ICollection<TEntity> collection)
+        {
+            throw new NotImplementedException();
+        }
+        protected virtual TDto MapToDto(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
