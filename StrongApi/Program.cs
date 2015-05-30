a9378378115040e63555f40838318c0090019187
Ali Sabzevari <ali@tidm.ir>
2015-05-30 15:04:55 +0430

    Initial commit

---
+++ StrongApi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace StrongApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:18080";
+            using (WebApp.Start<Startup>(url))
+            {
+                Process.Start(url); // Launch the browser.
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
