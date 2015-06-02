b5f3d94cc13513142bf76600a8815e9262cf2899
Ali Sabzevari <ali@tidm.ir>
2015-06-02 17:04:31 +0430

    Implemented a better GetId extension method (Issue #6)

---
+++ LiteApi.Tests/EntityExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.DataAnnotations;
+using LiteApi;
+
+namespace LiteApi.Tests
+{
+    [TestClass]
+    public class EntityExtensionsTests
+    {
+        [TestMethod]
+        public void GetId_must_find_property_with_name_Id()
+        {
+            var obj = new Entity1 { Id = 1 };
+            Assert.AreEqual(1, obj.GetId());
+        }
+        [TestMethod]
+        public void GetId_must_find_property_with_Key_attribute()
+        {
+            var obj = new Entity2 { ID = 1 };
+            Assert.AreEqual(1, obj.GetId());
+        }
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GetId_must_not_find_property_with_name_ID()
+        {
+            Exception expectedException = null;
+            try
+            {
+                var obj = new Entity3 { ID = 1 };
+                var id = obj.GetId();
+            }
+            catch (Exception ex)
+            {
+                expectedException = ex;
+            }
+            Assert.IsNotNull(expectedException);
+        }
+        [TestMethod]
+        public void GetId_must_find_property_with_Key_attribute_then_with_name_Key()
+        {
+            var obj = new Entity4 { Name = "1" };
+            Assert.AreEqual("1", obj.GetId());
+        }
+
+    }
+
+    public class Entity1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Entity2
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class Entity3
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Entity4
+    {
+        public int Id { get; set; }
+        [Key]
+        public string Name { get; set; }
+    }
+}
--- LiteApi/EntityExtensions.cs
+++ LiteApi/EntityExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,13 +13,25 @@ namespace LiteApi
     {
         public static object GetId(this object obj)
         {
-            var prop = obj.GetType().GetProperty("Id");
+            var prop = GetIdProperty(obj);
             return prop.GetValue(obj);
         }
         public static void SetId(this object obj, object value)
         {
-            var prop = obj.GetType().GetProperty("Id");
+            var prop = GetIdProperty(obj);
             prop.SetValue(obj, value);
+        }
+
+        private static PropertyInfo GetIdProperty(object obj)
+        {
+            var propsWithKeyAttribute = obj.GetType().GetProperties().Where(prop => prop.IsDefined(typeof(KeyAttribute))).ToArray();
+            if (propsWithKeyAttribute.Length > 1)
+                throw new Exception("More than one property with KeyAttribute defined.");
+            if (propsWithKeyAttribute.Length == 0)
+            {
+                return obj.GetType().GetProperty("Id");
+            }
+            return propsWithKeyAttribute[0];
         }
 
     }
